f365920b15b9d71e574d6dd21809cc71613333b4
volume4k <www.jonathan-kowa@gmx.de>
2015-10-17 13:09:31 +0200

    Ich habe den htdocs Serverordner hinzugefügt

---
+++ htdocs/index.php
@@ -0,0 +1,9 @@
+<?php
+if (isset($_COOKIE['firststart-foz'])){
+	header('Location: http://100.100.205.146/BS');
+}
+else {
+	SetCookie("firststart-foz");
+	header('Location: http://100.100.205.146/fs');
+}
+?>
