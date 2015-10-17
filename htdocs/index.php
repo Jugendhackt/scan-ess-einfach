af7076feb8760c5e8c43b2b5a7fff616c4a781ad
volume4k <www.jonathan-kowa@gmx.de>
2015-10-17 16:14:31 +0200

    Update index.php

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,9 +1,9 @@
 <?php
 if (isset($_COOKIE['firststart-foz'])){
-	header('Location: http://100.100.205.146/BS');
+	header('Location: /BS');
 }
 else {
 	SetCookie("firststart-foz");
-	header('Location: http://100.100.205.146/firststart');
+	header('Location: /firststart');
 }
 ?>
