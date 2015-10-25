5e75c666abd42b1220b503f860df273e3fa108e5
volume4k <www.jonathan-kowa@gmx.de>
2015-10-25 09:23:52 +0100

    Cookieverbesserungen + Namensänderung

--- htdocs/index.php
+++ htdocs/index.php
@@ -3,7 +3,7 @@ if (isset($_COOKIE['firststart-foz'])){
 	header('Location: /bs');
 }
 else {
-	SetCookie("firststart-foz");
+	SetCookie("firststart_isseseinfach");
 	header('Location: /firststart');
 }
 ?>
